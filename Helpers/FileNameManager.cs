77b3be588203c833da83e198b9146a58bb8e18ff
bayesiangraph <james_norman@hms.harvard.edu>
2020-05-07 09:43:08 -0400

    Revert "Revert "Fixed IE error with reading json data in client. It now works for all borwser types.""
    This reverts commit b491144f6478dd4b0ed814736d37e221708fc738.

---
+++ Helpers/FileNameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace i2b2_csv_loader.Helpers
+{
+    public static class FileNameManager
+    {
+        public static string FileName(string filename)
+        {
+            if (filename.Split("\\").Count() > 1)
+                filename = filename.Split("\\")[filename.Split("\\").Count() - 1];
+
+            return filename;
+
+        }
+    }
+}
